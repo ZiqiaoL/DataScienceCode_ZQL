b3f2b4e545b85726b76973a296a284e066309961
CherylLiu2016 <ziqiaoliu8709@gmail.com>
2017-02-15 14:46:40 -0500

    global file shiny

---
+++ Shiny_WorldHealthIndicator/global.R
@@ -0,0 +1,33 @@
+##global r
+
+library(dplyr)
+library(shiny)
+library(ggplot2)
+library(shiny)
+library(plotly)
+library(shinythemes)
+library(googleCharts)
+
+#use wide dataframe
+data <- read.csv('shiny_wide.csv', stringsAsFactors = F)
+data_long <- read.csv('shiny_long.csv', stringsAsFactors = F)
+
+trend_data <- subset(data_long,CountryName%in%c("United States","United Kingdom",
+                                                "United Arab Emirates", "Spain",
+                                                "Sweden","New Zealand",
+                                                "Ireland","Israel","Italy",
+                                                "Japan", "Mexico","Netherlands",
+                                                "India","France", "Germany",
+                                                "Canada","Chile","China",
+                                                "Colombia", "Australia", "Brazil") )
+trend_cn <- c("United States","United Kingdom",
+              "United Arab Emirates", "Spain",
+              "Sweden","New Zealand",
+              "Ireland","Israel","Italy",
+              "Japan", "Mexico","Netherlands",
+              "India","France", "Germany",
+              "Canada","Chile","China",
+              "Colombia", "Australia", "Brazil")
+
+
+
